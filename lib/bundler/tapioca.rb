60d240ad4d91acafa204384ad5baa76ffcd3876c
Andy Waite <andyw8@users.noreply.github.com>
2023-12-28 22:09:36 -0500

    $ bundle gem bundler-tapioca

---
+++ lib/bundler/tapioca.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tapioca/version"
+
+module Bundler
+  module Tapioca
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
