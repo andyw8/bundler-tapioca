d4e6cdfbe41464c0d566aba74e2ce52ecab77377
Andy Waite <andyw8@users.noreply.github.com>
2024-01-13 08:55:32 -0500

    Fixes

--- lib/bundler/tapioca.rb
+++ lib/bundler/tapioca.rb
@@ -3,5 +3,6 @@
 require_relative "tapioca/version"
 
 Bundler::Plugin.add_hook("after-install-all") do |dependencies|
+  puts "running tapioca..."
   system("bundle tapioca gem", exception: true)
 end
--- test/bundler/test_tapioca.rb
+++ test/bundler/test_tapioca.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class Bundle::TestTapioca < Minitest::Test
+class Bundler::TestTapioca < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Bundler::Tapioca::VERSION
   end
